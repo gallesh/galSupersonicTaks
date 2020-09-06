9079d60d79ea11e0a00fe941dc5d12d576f9786c
gallesh <leshemgal@gmail.com>
2020-09-06 12:35:21 +0300

    addToGitHub

---
+++ Assets/Scripts/ShowInterstitialScript.cs
@@ -0,0 +1,163 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+// Example for IronSource Unity.
+public class ShowInterstitialScript: MonoBehaviour
+{
+
+
+
+    public void Start()
+    {
+
+#if UNITY_ANDROID
+        string appKey = "85460dcd";
+#elif UNITY_IPHONE
+        string appKey = "8545d445";
+#else
+        string appKey = "unexpected_platform";
+#endif
+
+
+
+        Debug.Log("unity-script: IronSource.Agent.validateIntegration");
+        IronSource.Agent.validateIntegration();
+
+        Debug.Log("unity-script: unity version" + IronSource.unityVersion());
+
+        // SDK init
+        Debug.Log("unity-script: IronSource.Agent.init");
+        IronSource.Agent.init(appKey);
+
+    }
+
+    void OnEnable()
+    {
+
+        // Add Interstitial Events
+        IronSourceEvents.onInterstitialAdReadyEvent += InterstitialAdReadyEvent;
+        IronSourceEvents.onInterstitialAdLoadFailedEvent += InterstitialAdLoadFailedEvent;
+        IronSourceEvents.onInterstitialAdShowSucceededEvent += InterstitialAdShowSucceededEvent;
+        IronSourceEvents.onInterstitialAdShowFailedEvent += InterstitialAdShowFailedEvent;
+        IronSourceEvents.onInterstitialAdClickedEvent += InterstitialAdClickedEvent;
+        IronSourceEvents.onInterstitialAdOpenedEvent += InterstitialAdOpenedEvent;
+        IronSourceEvents.onInterstitialAdClosedEvent += InterstitialAdClosedEvent;
+    }
+
+    void OnApplicationPause(bool isPaused)
+    {
+        Debug.Log("unity-script: OnApplicationPause = " + isPaused);
+        IronSource.Agent.onApplicationPause(isPaused);
+    }
+
+    public void OnGUI()
+    {
+
+        GUI.backgroundColor = Color.blue;
+        GUI.skin.button.fontSize = (int)(0.035f * Screen.width);
+
+
+        Rect loadInterstitialButton = new Rect(0.10f * Screen.width, 0.35f * Screen.height, 0.35f * Screen.width, 0.08f * Screen.height);
+        if (GUI.Button(loadInterstitialButton, "Load Interstitial"))
+        {
+            Debug.Log("unity-script: LoadInterstitialButtonClicked");
+            IronSource.Agent.loadInterstitial();
+        }
+
+        Rect showInterstitialButton = new Rect(0.55f * Screen.width, 0.35f * Screen.height, 0.35f * Screen.width, 0.08f * Screen.height);
+        if (GUI.Button(showInterstitialButton, "Show Interstitial"))
+        {
+            Debug.Log("unity-script: ShowInterstitialButtonClicked");
+            if (IronSource.Agent.isInterstitialReady())
+            {
+                IronSource.Agent.showInterstitial();
+            }
+            else
+            {
+                Debug.Log("unity-script: IronSource.Agent.isInterstitialReady - False");
+            }
+        }
+
+
+    }
+
+    #region Interstitial callback handlers
+
+    void InterstitialAdReadyEvent()
+    {
+        Debug.Log("unity-script: I got InterstitialAdReadyEvent");
+    }
+
+    void InterstitialAdLoadFailedEvent(IronSourceError error)
+    {
+        Debug.Log("unity-script: I got InterstitialAdLoadFailedEvent, code: " + error.getCode() + ", description : " + error.getDescription());
+    }
+
+    void InterstitialAdShowSucceededEvent()
+    {
+        Debug.Log("unity-script: I got InterstitialAdShowSucceededEvent");
+    }
+
+    void InterstitialAdShowFailedEvent(IronSourceError error)
+    {
+        Debug.Log("unity-script: I got InterstitialAdShowFailedEvent, code :  " + error.getCode() + ", description : " + error.getDescription());
+    }
+
+    void InterstitialAdClickedEvent()
+    {
+        Debug.Log("unity-script: I got InterstitialAdClickedEvent");
+    }
+
+    void InterstitialAdOpenedEvent()
+    {
+        Debug.Log("unity-script: I got InterstitialAdOpenedEvent");
+    }
+
+    void InterstitialAdClosedEvent()
+    {
+        Debug.Log("unity-script: I got InterstitialAdClosedEvent");
+    }
+
+    /************* Interstitial DemandOnly Delegates *************/
+
+    void InterstitialAdReadyDemandOnlyEvent(string instanceId)
+    {
+        Debug.Log("unity-script: I got InterstitialAdReadyDemandOnlyEvent for instance: " + instanceId);
+    }
+
+    void InterstitialAdLoadFailedDemandOnlyEvent(string instanceId, IronSourceError error)
+    {
+        Debug.Log("unity-script: I got InterstitialAdLoadFailedDemandOnlyEvent for instance: " + instanceId + ", error code: " + error.getCode() + ",error description : " + error.getDescription());
+    }
+
+    void InterstitialAdShowFailedDemandOnlyEvent(string instanceId, IronSourceError error)
+    {
+        Debug.Log("unity-script: I got InterstitialAdShowFailedDemandOnlyEvent for instance: " + instanceId + ", error code :  " + error.getCode() + ",error description : " + error.getDescription());
+    }
+
+    void InterstitialAdClickedDemandOnlyEvent(string instanceId)
+    {
+        Debug.Log("unity-script: I got InterstitialAdClickedDemandOnlyEvent for instance: " + instanceId);
+    }
+
+    void InterstitialAdOpenedDemandOnlyEvent(string instanceId)
+    {
+        Debug.Log("unity-script: I got InterstitialAdOpenedDemandOnlyEvent for instance: " + instanceId);
+    }
+
+    void InterstitialAdClosedDemandOnlyEvent(string instanceId)
+    {
+        Debug.Log("unity-script: I got InterstitialAdClosedDemandOnlyEvent for instance: " + instanceId);
+    }
+
+
+
+
+    #endregion
+
+
+
+}
